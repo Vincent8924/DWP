64545d4d4f04314c91f24e15b862adeb5e004a64
CHEE JIONG KING <149455482+CHEEJIONGKING@users.noreply.github.com>
2024-02-04 11:59:38 +0800

    hotel.sql

--- hotel.sql
+++ hotel.sql
@@ -3,6 +3,14 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+CREATE TABLE `Contact` (
+  `first_name` varchar(200) NOT NULL,
+  `last_name` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `phone` varchar(200) NOT NULL,
+  `message` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 CREATE TABLE `Payment` (
   `first_name` varchar(200) NOT NULL,
   `last_name` varchar(200) NOT NULL,
@@ -13,4 +21,12 @@ CREATE TABLE `Payment` (
   `card_number` int(200) NOT NULL,
   `card_expire_date` varchar(200) NOT NULL,
   `card_CVV` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `Rating & Comment` (
+  `first_name` varchar(200) NOT NULL,
+  `last_name` varchar(200) NOT NULL,
+  `room_type` varchar(200) NOT NULL,
+  `rating` int NOT NULL,
+  `comment` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
