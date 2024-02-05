f8f91aadcca7bc431f1b1703ba6724ab75137382
LOJINKAI <1221201364@student.mmu.edu.my>
2024-02-05 13:56:59 +0800

    upload

--- room_category.sql
+++ room_category.sql
@@ -5,21 +5,16 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `room_category` (
   `room_type` varchar(100) NOT NULL,
-  `room_quantity` INT NOT NULL
+  `room_quantity` INT NOT NULL,
+  `picture` varchar(100) NOT NULL,
+  `price ` INT NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 INSERT INTO `room_category`(`room_type`, `room_quantity`, `picture`, `price`) 
-VALUES ('Presidential Suite','5','room a/room a 1.png','500')
-
-INSERT INTO `room_category`(`room_type`, `room_quantity`, `picture`, `price`) 
-VALUES ('Specialty Suite','5','room b/room b 1.png','400')
-
-INSERT INTO `room_category`(`room_type`, `room_quantity`, `picture`, `price`) 
-VALUES ('Executive Suite','5','room c/room c 1.png','300')
-
-INSERT INTO `room_category`(`room_type`, `room_quantity`, `picture`, `price`) 
-VALUES ('Club Premier Room','5','room d/room d 1.png','200')
-
-INSERT INTO `room_category`(`room_type`, `room_quantity`, `picture`, `price`) 
-VALUES ('Deluxe Rooms','5','room e/room e 1.png','100')
+VALUES 
+  ('Presidential Suite', '5', 'room a/room a 1.png', '500'),
+  ('Specialty Suite', '5', 'room b/room b 1.png', '400'),
+  ('Executive Suite', '5', 'room c/room c 1.png', '300'),
+  ('Club Premier Room', '5', 'room d/room d 1.png', '200'),
+  ('Deluxe Rooms', '5', 'room e/room e 1.png', '100');
