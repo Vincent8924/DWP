b0e6ddda0b0c524f296d96aa2820a8afc30625de
CHEE JIONG KING <149455482+CHEEJIONGKING@users.noreply.github.com>
2024-02-06 12:42:38 +0800

    Update user_menu.php

--- user_menu.php
+++ user_menu.php
@@ -28,6 +28,7 @@
             echo "<a  href='room list.php'>ROOM</a>";
             echo "<a href='aboutus.php'>ABOUT</a>";
             echo "<a href='contact.php'>CONTACT</a>";
+            echo "<a href='rating&comment.php'>RATING & COMMENT</a>";
             echo "<a href='shopping cart.php'>CART</a>";
             ?>
         </div>
