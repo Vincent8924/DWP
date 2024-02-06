3994dc89dc6d6c0e14bac68a4bb1ad6f1201ee61
CHEE JIONG KING <149455482+CHEEJIONGKING@users.noreply.github.com>
2024-02-06 12:33:24 +0800

    Add files via upload

---
+++ user_menu.php
@@ -0,0 +1,36 @@
+<style>
+#line {
+    border: 0px;
+    background-image: url('image/black.png');
+    background-repeat: repeat-x;
+    height: 40px;
+}
+
+.choice a {
+    font-size: 20px;
+    text-decoration: none;
+    color: white;
+    margin-left: 100px;
+    position: relative;
+    top: 8px;
+    display: inline-block; 
+}
+
+</style>
+
+<body>
+    <img src="image/KuanTan Hotels2.png" style="height: 50px;" />
+    <br />
+    <div id="line">
+        <div class="choice">
+            <?php
+            echo "<a href='index.php'>HOME</a>";
+            echo "<a  href='room list.php'>ROOM</a>";
+            echo "<a href='aboutus.php'>ABOUT</a>";
+            echo "<a href='contact.php'>CONTACT</a>";
+            echo "<a href='shopping cart.php'>CART</a>";
+            ?>
+        </div>
+    </div>
+    <br/><br/>
+</body>
