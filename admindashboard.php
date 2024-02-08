457ce7ba6e8996bca8ecc7036ede65393802c68b
Vincent <1221202213@student.mmu.edu.my>
2024-02-09 02:27:28 +0800

    commit

--- admindashboard.php
+++ admindashboard.php
@@ -60,18 +60,21 @@
             echo"torder error";
         }
     ?>
-        
-
         <div class="grid">
-            <div class="grid_container ">
+            
+            <div id="member" class="grid_container ">
+                <img src="memberlogo.jpn" alt="member">
                 <h2>
                     <?php echo $tmember?>
                 </h2>
-                <p>
-                    member
-                </p>
+                <a href="member.php" id="member">
+                    <p>
+                        member
+                    </p>
+                </a>
+                
             </div>
-            <div class="grid_container ">
+            <div id="admin" class="grid_container ">
                 <h2>
                     <?php echo $tadmin?>
                 </h2>
@@ -79,7 +82,7 @@
                     admin
                 </p>
             </div>
-            <div class="grid_container ">
+            <div id="room" class="grid_container ">
                 <h2>
                     <?php echo $troom?>
                 </h2>
@@ -87,7 +90,7 @@
                     Room
                 </p>
             </div>
-            <div class="grid_container ">
+            <div id="order"class="grid_container ">
                 <h2>
                     <?php echo $torder?>
                 </h2>
@@ -98,56 +101,8 @@
         </div>
 
 
-        <div class="chart">
-            <canvas id="saleChart"></canvas>
-        </div>
+        
         
         
     </body>
-    <script>
-        const saleData = [19, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    
-        // Chart.js configuration
-        const ctx = document.getElementById("saleChart").getContext("2d");
-        const saleChart = new Chart(ctx, {
-            type: "line",
-            data: {
-                labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "August", "September", "October", "November", "December"],
-                datasets: [
-                    {
-                        label: "Sale Report",
-                        borderColor: "#3498db",
-                        backgroundColor: "rgba(52, 152, 219, 0.2)",
-                        data: saleData,
-                    },
-                ],
-            },
-            options: {
-                plugins: {
-                    title: {
-                        display: true,
-                        text: 'Monthly Sales Report 2023'
-                        
-                    },
-                },
-                scales: {
-                    x: {
-                        title: {
-                            display: true,
-                            text: 'Month'
-                        }
-                    },
-                    y: {
-                        title: {
-                            display: true,
-                            text: 'Sales(000)'
-                        },
-                        
-                        
-                    }
-                }
-            }
-        });
-    </script>
-      
     
